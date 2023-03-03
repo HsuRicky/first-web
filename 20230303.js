367d47465b4cce10cb851358cda70ee4e2ff4d00
HsuRicky <ricky850405@gmail.com>
2023-03-03 17:34:10 +0800

    20230303

--- 20230303.js
+++ 20230303.js
@@ -1,6 +1,6 @@
 //console.log("HI")
 
-let example = [1,2,3,5,8,11,19,30,'49']
+let example = [1,2,3,5,8,11,19,30,49,79,128]
 
 //console.log( typeof example )
 
@@ -9,9 +9,33 @@ let example = [1,2,3,5,8,11,19,30,'49']
 //console.log( typeof true )
 
 
-
+/*
 example.forEach((item)=>{
     console.log(item)
     console.log( typeof item )
     //console.log( typeof x )
 })
+*/
+
+/*
+let odd = [] , even = []
+example.forEach((item)=>{
+    if(item%2==0) even.push(item);
+    else odd.push(item)
+})
+console.log( "odd:",odd )
+console.log( "even:",even )
+*/
+
+/*
+function multiply(a,b,c){
+    return a*b*c
+}
+console.log( multiply(1,2,3) )
+*/
+
+let multiply_other = (a) => (b,c) =>{
+    return a * (b + c)
+}
+console.log( multiply_other(10)(3,8) )
+console.log( typeof multiply_other)
