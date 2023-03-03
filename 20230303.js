101e78014a181f1ceb8b8cc89137dea1c25a80c1
HsuRicky <ricky850405@gmail.com>
2023-03-03 14:19:08 +0800

    practice

---
+++ 20230303.js
@@ -0,0 +1,17 @@
+//console.log("HI")
+
+let example = [1,2,3,5,8,11,19,30,'49']
+
+//console.log( typeof example )
+
+//console.log( typeof 5 )
+
+//console.log( typeof true )
+
+
+
+example.forEach((item)=>{
+    console.log(item)
+    console.log( typeof item )
+    //console.log( typeof x )
+})
